19a9e83ae3b9684a81f04fad676e470ef075e8ae
ali <ali@sonono.ch>
2022-02-26 23:34:13 +0200

    edit readme file and handle some code in checkout file

--- README.md
+++ README.md
@@ -14,3 +14,4 @@ this is an online bookstore where the user can view, search and purchase their f
 * Run in you terminal / CMD: `docker-compose up` (make sure you are in the project directory)
 * The server should be now running under `http://127.0.0.1:8000/` (if port 8000 is available)
 * Admin dashboard is located in `http://127.0.0.1:8000/admin` and the admin credentials are: `admin@admin.com`, `admin@1234`.
+* To Run Custom Command, like django shell, just type: `docker-compose run web sh -c "YOUR COMMAND HERE"`.
--- project/store/views/checkout.py
+++ project/store/views/checkout.py
@@ -1,6 +1,8 @@
 from decimal import Decimal
+
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from project.store.forms import CheckoutForm
 from project.store.models import Cart, Order, ORDER_STATUS, Address, Payment, OrderItem
@@ -9,7 +11,7 @@ import stripe, os
 
 stripe.api_key = os.environ.get('STRIPE_SECRET_KEY')
 
-
+@login_required
 def checkout(request):
     """a view for checkout process"""
     form = CheckoutForm()    
@@ -24,42 +26,46 @@ def checkout(request):
     return render(request, 'checkout.html', context)
 
 
+@login_required
 def pay(request):
     """a view for paying"""
     if request.method == 'POST':
-        address = Address.objects.filter(user=request.user).select_related('user').last()
-        cart = Cart.objects.get(user=request.user)
-        cart_items = cart.items.all()
-        charge = stripe.PaymentIntent.create(
-            amount=int(cart.get_cart_total() * 100),
-            currency="usd",
-            automatic_payment_methods={
-                'enabled': True,
-            },
-            description="charged from test account"
-        )
-        order = Order.objects.create(
-            user=request.user,
-            total=Decimal(cart.get_cart_total()),
-            order_status=ORDER_STATUS.ORDERED,
-            address=address
-        )
-        for cart_item in cart_items:
-            order_item=OrderItem.objects.create(
-                item=cart_item.item,
-                quantity=cart_item.quantity,
-                user=request.user
+        try:
+            address = Address.objects.filter(user=request.user).select_related('user').last()
+            cart = Cart.objects.get(user=request.user)
+            cart_items = cart.items.all()
+            charge = stripe.PaymentIntent.create(
+                amount=int(cart.get_cart_total() * 100),
+                currency="usd",
+                automatic_payment_methods={
+                    'enabled': True,
+                },
+                description="charged from test account"
             )
-            order.items.add(order_item)
-            cart_item.delete()
-        cart.delete()
+            order = Order.objects.create(
+                user=request.user,
+                total=Decimal(cart.get_cart_total()),
+                order_status=ORDER_STATUS.ORDERED,
+                address=address
+            )
+            for cart_item in cart_items:
+                order_item=OrderItem.objects.create(
+                    item=cart_item.item,
+                    quantity=cart_item.quantity,
+                    user=request.user
+                )
+                order.items.add(order_item)
+                cart_item.delete()
+            cart.delete()
 
-        # create the payment
-        Payment.objects.create(
-        stripe_charge_id = charge['id'],
-        user = request.user,
-        amount = order.total
-        )
-        messages.success(request, "Your order was successful!")
-        return redirect("/")
+            # create the payment
+            Payment.objects.create(
+            stripe_charge_id = charge['id'],
+            user = request.user,
+            amount = order.total
+            )
+            messages.success(request, "Your order is created successfully!")
+            return redirect("/")
+        except:
+            messages.warning(request, "something wrong!")
     return render(request, 'pay.html')
